bbd247a91cac411cdc665f65abd24614badd71ee
unknown <pranavkumar0712@gmail.com>
2026-07-17 10:16:47 +0530

    Force-dynamic home page to fetch live Supabase data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { BioluminescenceBackground } from "@/components/webgl/BioluminescenceBac
 import { DigitalRainPreloader } from "@/components/DigitalRainPreloader"
 import { getAllProjects } from "@/lib/projects"
 
+export const dynamic = "force-dynamic"
+
 export default async function Home() {
   const projects = await getAllProjects()
 
